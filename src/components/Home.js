43b1481e59f62a08a469be81eab551267f77ea87
Manthan Kumbhar <kum.manthan@gmail.com>
2021-07-15 13:21:09 +0530

    sidebar edits

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import { Redirect } from "react-router";
 import "./Home.css";
-import home from "../Photos/home.png";
-import search from "../Photos/magnifying-glass.png";
-import map from "../Photos/map.png";
+import sprite from "../Photos/SVG/sprite.svg";
+import logo from "../Photos/logo.png";
+import { Link } from "react-router-dom";
 
 class Home extends Component {
   constructor() {
@@ -26,25 +26,48 @@ class Home extends Component {
       <div className="content_main">
         <nav className="sidebar">
           <ul className="nav_ul">
+            <Link to="/" className="nav_logo_main">
+              <img src={logo} className="nav_logo" alt="logo" />
+            </Link>
             <li className="nav_li">
               <a className="li_item" href="/">
-                <img src={home} className="nav_icon" alt="nav icons" />
+                <svg className="nav_icon">
+                  <use href={sprite + "#icon-home"} />
+                </svg>
                 Home
               </a>
             </li>
             <li className="nav_li">
               <a className="li_item" href="/">
-                <img src={search} className="nav_icon" alt="nav icons" />
+                <svg className="nav_icon">
+                  <use href={sprite + "#icon-magnifying-glass"} />
+                </svg>
                 Search
               </a>
             </li>
             <li className="nav_li">
               <a className="li_item" href="/">
-                <img src={map} className="nav_icon" alt="nav icons" />
+                <svg className="nav_icon">
+                  <use href={sprite + "#icon-map"} />
+                </svg>
                 Browse
               </a>
             </li>
           </ul>
+          <div className="nav_legal">
+            {" "}
+            Developed by{" "}
+            <a
+              href="https://manthankumbhar.netlify.app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Manthan Kumbhar
+            </a>{" "}
+            &copy; {new Date().getFullYear()}
+            <br />
+            All Rights Reserved
+          </div>
         </nav>
         <div className="content">
           <button className="btn btn-warning button_home" onClick={this.logout}>
