8d5bb77e232de91dbfd27ba361bf1dfa4e940f72
manthankumbhar <kum.manthan@gmail.com>
2021-10-21 14:41:18 +0530

    player responsive changes

--- src/hoc/HocPlayer/HocPlayer.js
+++ src/hoc/HocPlayer/HocPlayer.js
@@ -1,18 +1,29 @@
 import React from "react";
-import AudioPlayer from "react-h5-audio-player";
+import AudioPlayer, { RHAP_UI } from "react-h5-audio-player";
 import "react-h5-audio-player/src/styles.scss";
 import "./HocPlayer.scss";
 
 function HocPlayer(props) {
   return (
     <div className="hocplayer">
-      <AudioPlayer autoPlay src={props.src} className="hocplayer__player" />
-      <h1 className="hocplayer__header">
-        Playing:{" "}
-        {props.songName === "" || props.songName === undefined
-          ? "None"
-          : props.songName}
-      </h1>
+      <AudioPlayer
+        autoPlay
+        src={props.src}
+        className="hocplayer__player"
+        customAdditionalControls={[
+          <h1 className="hocplayer__header">
+            Playing:{" "}
+            {props.songName === "" || props.songName === undefined
+              ? "None"
+              : props.songName}
+          </h1>,
+        ]}
+        customControlsSection={[
+          RHAP_UI.ADDITIONAL_CONTROLS,
+          RHAP_UI.MAIN_CONTROLS,
+          RHAP_UI.VOLUME_CONTROLS,
+        ]}
+      />
     </div>
   );
 }
